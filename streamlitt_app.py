64d229e5ffc84231f3912207451abd24ca93b6ba
Leladro <134005442+Leladro@users.noreply.github.com>
2023-05-18 15:04:29 -0400

    Update streamlitt_app.py
    Adding plates

--- streamlitt_app.py
+++ streamlitt_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
-streamlit.title("My parents new healty diner")
+streamlit.title('My parents new healty diner')
 
 streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+steamlit.text('Hard-Bouled Free Range Egg')
